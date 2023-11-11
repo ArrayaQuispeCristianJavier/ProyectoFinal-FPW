6df2904c09b7f50dd4e9c4c25325cdd3e540f5c1
ArrayaQuispeCristianJavier <arrayacristian75@gmail.com>
2023-11-10 21:19:44 -0300

    Comparador precio se a modificado y ordenado paso por paso

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Productos from './components/Producto';
 function App() {
     /*El componente ruteador que contiene el componente rutas y describe cada una de ellas osea de las rutas*/
     return (
@@ -13,19 +14,19 @@ function App() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="comparadorPrecio">Comparador de precio</Nav.Link>
-                            <Nav.Link href="dude">Dude</Nav.Link>
-                            <Nav.Link href="naveEspacial">Nave espacial</Nav.Link>
-                            <Nav.Link href="listaTarea">Lista de tarea</Nav.Link>
-                            <Nav.Link href="juegoNino">Juego para ninos</Nav.Link>
-                            <Nav.Link href="integrantes">Integrantes</Nav.Link>
+
+                            <Nav.Link href="/comparadorPrecio">Comparador de precio</Nav.Link>
+                            <Nav.Link href="/dude">Dude</Nav.Link>
+                            <Nav.Link href="/naveEspacial">Nave espacial</Nav.Link>
+                            <Nav.Link href="/listaTarea">Lista de tarea</Nav.Link>
+                            <Nav.Link href="/juegoNino">Juego para ninos</Nav.Link>
+                            <Nav.Link href="/integrantes">Integrantes</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
             <Routes>
-                {/* <Route path='comparadorPrecio' element={}/> */}
-                
+                <Route path='comparadorPrecio' element={<Productos/>}/>          
             </Routes>
         </Router>
     )
